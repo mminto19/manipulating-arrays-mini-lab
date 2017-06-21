bff1a8806a43642b6cd1e913bb1dbd4f3dbae016
McKenzie Minto <mminto19@students.polytechnic.org>
2017-06-21 18:39:16 +0000

    done!

--- countries_array.rb
+++ countries_array.rb
@@ -1,1 +1,14 @@
+# =>                    0       1           2           3       4       5           6
+four_letter_countries=["Chad", "Cuba" , "Greenland" , "Iraq" , "Mali" , "Oman" , "India"]
+p four_letter_countries=four_letter_countries.push("Fiji")
+p four_letter_countries=four_letter_countries.insert(4 , "Iran")
+p four_letter_countries.delete("India")
+p four_letter_countries
+p four_letter_countries=four_letter_countries.reverse
+p four_letter_countries[5] = "Togo"
+p four_letter_countries
+p four_letter_countries=four_letter_countries.push("Laos")
+p four_letter_countries=four_letter_countries.reverse
+p four_letter_countries=four_letter_countries.insert(0, "Peru")
+
 
